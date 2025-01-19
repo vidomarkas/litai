d30856604bab2142a0b2654537bd421bc77e6344
Viktoras Domarkas <vidomarkas@gmail.com>
2025-01-19 13:43:33 +0000

    create tabs

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Text>Budget Tracker</Text>
+			<Link href="/sign-in">Sign In</Link>
+			<Link href="/profile">Profile</Link>
+		</View>
+	);
+}
